da9b3c87d52a0c89577edd4181293dcbb0c1719a
SerhiiVynnyk <sprtan1z@gmail.com>
2024-11-18 11:49:18 +0200

    replaced wrong parametr

--- cypress/e2e/main_page.cy.js
+++ cypress/e2e/main_page.cy.js
@@ -7,7 +7,7 @@ describe('Main page tests', () => {
   
   it('Check the presence of Main page menu elements ', () => {
     cy.visit('https://telnyx.com');
-    mainPage.checkElementDisplayedByName(mainPage.productsItem, mainPageTestData.pricingItem);
+    mainPage.checkElementDisplayedByName(mainPage.productsItem, mainPageTestData.productsItem);
     mainPage.checkElementDisplayedByName(mainPage.solutionsItem, mainPageTestData.solutionsItem);
     mainPage.checkElementDisplayedByName(mainPage.pricingItem, mainPageTestData.pricingItem);
     mainPage.checkElementDisplayedByName(mainPage.whyTelnyxItem, mainPageTestData.whyTelnyxItem);
